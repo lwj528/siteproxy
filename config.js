3ece39c4b66d6b946c636218d28e23803fd37233
lwj528 <67459448+lwj528@users.noreply.github.com>
2022-07-19 17:21:23 +0800

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ const {CookieAccessInfo, CookieJar, Cookie} = cookiejar
 
 let config = {
     httpprefix: 'https', port: 443,
-    serverName: 'siteproxy-kappa.vercel.app',
+    serverName: 'sleepy-lake-77489.herokuapp.com',
 }
 let blockedSites = ['www.chase.com'] // accessing to chase.com was reported by google
 
